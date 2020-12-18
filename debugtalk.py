7f9c3b861ebb3d2e74124b5c0d5eb19aa10fd2a9
yanzi <1129166485@qq.com>
2020-12-18 22:15:27 +0800

    校验数据库

--- debugtalk.py
+++ debugtalk.py
@@ -1,4 +1,6 @@
+from pymysql.cursors import DictCursor
 
+from common import mysql_handler, excel_hander
 import time
 
 
@@ -15,5 +17,26 @@ def get_phone():
     return phone
 
 
+def read_excel_data():
+    data_path = r"F:\python\python_hrun\data\testdata.xlsx"
+    data = excel_hander.ExcelHandler(data_path).read_sheet_data("register")
+    return data
+
+
+def get_sql_data():
+    SQL = mysql_handler.MySQLHandler(
+        host="120.78.128.25",
+        port=3306,
+        user="future",
+        password="123456",
+        charset="utf8",
+        cursorclass=DictCursor
+    )
+    phone_sql = "select mobile_phone from futureloan.member where  mobile_phone={};".format(read_excel_data()[0]["mobile_phone"])
+    phone_recode = SQL.select_data(sql=phone_sql)
+    if phone_recode:
+        return phone_recode
+
+
 if __name__ == '__main__':
-    print(get_phone())
+    print(get_sql_data())
